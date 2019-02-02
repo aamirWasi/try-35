5252ffa6cf71fbbbd5620c881ed5533d2b2363e6
aamir <wasi3aamir@gmail.com>
2019-02-02 22:24:46 +0600

    rabbish problem

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int t = 1; t <= T; t++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split('\n');
+                int num = int.Parse(space[0]);
+                bool SquarePerfect = IsperfectSquare(num);
+                if (SquarePerfect)
+                {
+                    Console.WriteLine("Case {0}: YES", t);
+                }
+                else
+                {
+                    Console.WriteLine("Case {0}: NO", t);
+                }
+            }
+        }
+        public static bool IsperfectSquare(int n)
+        {
+            int root = (int)Math.Sqrt(n);
+            return (int)Math.Pow(root, 2) == n;
+        }
+    }
+}
